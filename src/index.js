da2687833f938ea1efc18b820be0f37e3e6d30db
Guilherme Lima de Brito <guilhermelb@take.net>
2020-03-03 14:13:02 -0300

    Change the index components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+import { BlipCard, BlipCardContainer } from "./components/BlipCard";
+import { BlipInputReact } from "./components/BlipInput";
+import { BlipLoading } from "./components/BlipLoading";
+import { BlipPageHeader } from "./components/PageHeader";
+
+export {
+  BlipCard,
+  BlipCardContainer,
+  BlipInputReact,
+  BlipLoading,
+  BlipPageHeader
+};
